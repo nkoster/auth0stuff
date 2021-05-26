c7ff78fc6e68b9e15d7b2456e51e369336287efa
Niels Koster <niels.koster@portavita.nl>
2021-05-26 14:41:34 +0200

    Add requiresAuth and routing

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express()
 
 require('dotenv').config()
 
-const { auth } = require('express-openid-connect')
+const { auth, requiresAuth } = require('express-openid-connect')
 
 app.use(
   auth({
+    authRequired: false,
+    auth0Logout: true,
     issuerBaseURL: process.env.ISSUER_BASE_URL,
     baseURL: process.env.BASE_URL,
     clientID: process.env.CLIENT_ID,
@@ -14,6 +16,14 @@ app.use(
     idpLogout: true,
   })
 )
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'logged in' : 'logged out')
+})
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(req.oidc.user)
+})
 
 const port = process.env.PORT || 3005
 
